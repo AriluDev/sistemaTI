484e68086717638f5f89bad894df268392caa9da
Arilu <ariwayne@proton.me>
2026-03-09 00:04:01 -0500

    Se deben aplicar mejoras

--- src/main/java/com/mounstros_de_la_computacion/sistemaTI/model/repository/TecnicoRepositorio.java
+++ src/main/java/com/mounstros_de_la_computacion/sistemaTI/model/repository/TecnicoRepositorio.java
@@ -1,5 +1,8 @@
 package com.mounstros_de_la_computacion.sistemaTI.model.repository;
 
-public class TecnicoRepositorio {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.mounstros_de_la_computacion.sistemaTI.model.entity.Tecnico;
+
+public interface TecnicoRepositorio extends JpaRepository<Tecnico, Long> {
+
 }
