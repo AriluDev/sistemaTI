7329e1c83b62f98437a89754165a02b0be74c3da
Arilu <ariwayne@proton.me>
2026-03-09 20:59:57 -0500

    Nuevo proyecto

---
+++ src/test/java/com/mounstros_de_la_computacion/sistemaTI/SistemaTiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mounstros_de_la_computacion.sistemaTI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaTiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
